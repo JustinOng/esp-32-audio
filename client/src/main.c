4f9ad7887ae547c5e4eae148cd5782f8dbf9475e
Justin Ong <justin.ong.97@gmail.com>
2017-05-15 19:48:31 +0800

    added audio isr and *other things*

--- client/src/main.c
+++ client/src/main.c
@@ -10,15 +10,26 @@
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "tcpip_adapter.h"
+#include "timer.h"
+#include <lwip/sockets.h>
 
-#define AP_SSID "ESP32 Test"
-#define AP_PASSWORD "network password"
+#define PORT_NUMBER 8080
+
+// for audio isr
+#define TIMER_DIVIDER 1
+#define TRK_TIMER_GROUP	TIMER_GROUP_0
+#define TRK_TIMER_IDX	TIMER_1
+// technically is 1814.05, what do i do with the .05? track and increment when it exceeds 1 count?
+#define AUDIO_ISR_INTERVAL 1814
 
 EventGroupHandle_t wifi_event_group;
 
 const int CONNECTED_BIT = BIT0;
 
 static const char* TAG = "TEST";
+
+const char* AP_SSID = "ESP32 Test";
+const char* AP_PASSWORD = "hello123";
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
@@ -58,9 +69,10 @@ static void initialise_wifi(void)
   // docs says default 4 max 4 but without explicitly setting it, i keep getting max connections?
   ap_cfg.ap.max_connection = 4;
   // memcpy instead of strcpy because ap.ssid and ap.password are declared as uint8_t for some reason :(
-  memcpy(ap_cfg.ap.ssid, AP_SSID, strlen(AP_SSID));
-  memcpy(ap_cfg.ap.password, AP_PASSWORD, strlen(AP_PASSWORD));
-  ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", ap_cfg.ap.ssid);
+  // strlen() + 1 because strlen does not copy the null terminating character
+  memcpy(ap_cfg.ap.ssid, AP_SSID, strlen(AP_SSID)+1);
+  memcpy(ap_cfg.ap.password, AP_PASSWORD, strlen(AP_PASSWORD)+1);
+  ESP_LOGI(TAG, "Setting WiFi configuration %s/%s...", ap_cfg.ap.ssid, ap_cfg.ap.password);
 
   ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_AP, &ap_cfg) );
   ESP_ERROR_CHECK( esp_wifi_start() );
@@ -69,25 +81,79 @@ static void initialise_wifi(void)
   IP4_ADDR(&ipInfo.ip, 192,168,1,1);
   IP4_ADDR(&ipInfo.gw, 192,168,1,1);
   IP4_ADDR(&ipInfo.netmask, 255,255,255,0);
-  tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &ipInfo);
+  ESP_ERROR_CHECK(tcpip_adapter_dhcps_stop(TCPIP_ADAPTER_IF_AP));
+  ESP_ERROR_CHECK(tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_AP, &ipInfo));
+  ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
 }
 
-static void wpa2_enterprise_example_task(void *pvParameters)
+void IRAM_ATTR timerIsr(void *para)
 {
-  tcpip_adapter_ip_info_t ip;
-  memset(&ip, 0, sizeof(tcpip_adapter_ip_info_t));
-  vTaskDelay(2000 / portTICK_PERIOD_MS);
+  static int i=0;
+  ESP_LOGI(TAG, "ISR %d", i++);
+
+  // clear alarm?
+  TIMERG0.int_clr_timers.t1 = 1;
+  TIMERG0.hw_timer[TRK_TIMER_IDX].config.alarm_en = 1;
+}
+
+static void initialise_audio_isr(void) {
+  // https://www.esp32.com/viewtopic.php?t=1094
+  timer_config_t config;
+  config.alarm_en = 1;
+  config.auto_reload = 1;
+  config.counter_dir = TIMER_COUNT_UP;
+  config.divider = TIMER_DIVIDER;
+  config.intr_type = TIMER_INTR_LEVEL;
+  config.counter_en = TIMER_PAUSE;
+
+  timer_init(TRK_TIMER_GROUP, TRK_TIMER_IDX, &config);
+  timer_set_counter_value(TRK_TIMER_GROUP, TRK_TIMER_IDX, 0x00000000ULL);
+  timer_enable_intr(TRK_TIMER_GROUP, TRK_TIMER_IDX);
+  timer_isr_register(TRK_TIMER_GROUP, TRK_TIMER_IDX, timerIsr, NULL, ESP_INTR_FLAG_IRAM, NULL);
+
+  timer_pause(TRK_TIMER_GROUP, TRK_TIMER_IDX);
+  timer_set_counter_value(TRK_TIMER_GROUP, TRK_TIMER_IDX, 0x00000000ULL);
+  timer_set_alarm_value(TRK_TIMER_GROUP, TRK_TIMER_IDX, AUDIO_ISR_INTERVAL);
+  timer_start(TRK_TIMER_GROUP, TRK_TIMER_IDX);
+}
+
+static void listen_audio_data(void *pvParameters)
+{
+  // https://github.com/nkolban/esp32-snippets/blob/master/sockets/server/socket_server.c
+  struct sockaddr_in clientAddress;
+  struct sockaddr_in serverAddress;
+
+  // http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
+  // Create a socket that we will listen upon.
+  // AF_INET: ipv4
+  // SOCK_DGRAM & IPPROTO_UDP: self explanatory
+  // use SOCK_STREAM, IPPROTO_TCP for tcp
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 0) {
+    ESP_LOGE(TAG, "socket: %d %s", sock, strerror(errno));
+    return;
+  }
+
+  // Bind our server socket to a port.
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddress.sin_port = htons(PORT_NUMBER);
+  int rc  = bind(sock, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+  if (rc < 0) {
+    ESP_LOGE(TAG, "bind: %d %s", rc, strerror(errno));
+    return;
+  }
+
+  // Flag the socket as listening for new connections.
+  rc = listen(sock, 5);
+  if (rc < 0) {
+    ESP_LOGE(TAG, "listen: %d %s", rc, strerror(errno));
+    return;
+  }
 
   while (1) {
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
 
-    if (tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP , &ip) == 0) {
-      ESP_LOGI(TAG, "~~~~~~~~~~~");
-      ESP_LOGI(TAG, "IP:"IPSTR, IP2STR(&ip.ip));
-      ESP_LOGI(TAG, "MASK:"IPSTR, IP2STR(&ip.netmask));
-      ESP_LOGI(TAG, "GW:"IPSTR, IP2STR(&ip.gw));
-      ESP_LOGI(TAG, "~~~~~~~~~~~");
-    }
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
   }
 }
 
@@ -95,5 +161,5 @@ void app_main()
 {
   ESP_ERROR_CHECK( nvs_flash_init() );
   initialise_wifi();
-  xTaskCreate(&wpa2_enterprise_example_task, "wpa2_enterprise_example_task", 4096, NULL, 5, NULL);
+  xTaskCreate(&listen_audio_data, "listen_audio_data", 4096, NULL, 5, NULL);
 }
